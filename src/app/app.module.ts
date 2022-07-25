a20f6c10f954720eaa1231ed1632e690398c208b
Luis Miguel <2120100449@soy.utj.edu.mx>
2022-07-25 10:37:42 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Vibration } from '@awesome-cordova-plugins/vibration/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: Vibration }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Vibration } from '@awesome-cordova-plugins/vibration/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  slides = [
+    {
+      img: 'assets/img/casa1.jpg',
+      titulo: 'Seguridad <br> la mejor opción para el hogar',
+    },
+    {
+      img: 'assets/img/casa2.jpg',
+      titulo: 'Acecibilidad<br>para cualquier econimia',
+    },
+    {
+      img: 'assets/img/casa3.jpg',
+      titulo: 'Confiabilidad<br> certeza y confianza',
+    },
+  ];
+  constructor(private vibration: Vibration) { }
+  vibrar() {
+    this.vibration.vibrate([2000, 1000, 2000]);
+  }
 
 }
